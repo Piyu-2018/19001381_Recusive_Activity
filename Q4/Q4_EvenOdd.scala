7239338ce3514fc88f5437d6d20c42437ea73ec7
Piyumi Rathnayaka <2019cs138@stu.ucsc.lk>
2021-07-18 13:16:09 +0700

    Even odd numbers

---
+++ Q4/Q4_EvenOdd.scala
@@ -0,0 +1,11 @@
+object Q4_EvenOdd extends App{
+  def isEven(x:Int):Boolean = x match{
+    case 0 => true
+    case _  => isOdd(x-1)
+  }
+
+  def isOdd(x:Int):Boolean = !isEven(x)
+
+  println(isEven(8))
+  println(isOdd(5))
+}
